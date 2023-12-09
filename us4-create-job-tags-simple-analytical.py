a39667b705124bba85f05ae2e491b7d95ed303c2
sophiajwitt <sophiawitt33@gmail.com>
2023-12-09 10:49:51 -0500

    us4 complete

--- us4-create-job-tags-simple-analytical.py
+++ us4-create-job-tags-simple-analytical.py
@@ -10,22 +10,20 @@ So That:  The job search is less cluttered and maintains relevance to the user
 
 print(us)
 
-def create_job_tags():
+def create_job_tags(level):
 
-    # cols = 'b.bid b.severity i.initial_date i.product i.status i.priority u.uid u.name u.role'
+    cols = 'jobID name level salary location requirement description companyID'
 
-#     tmpl =  f'''
-# SELECT {c(cols)}
-#   FROM Bugs as b
-#        JOIN Issues as i ON b.bid = i.iid
-#        JOIN Users as u ON i.uid = u.uid
-#  WHERE i.status <> 'resolved'
-#  ORDER BY b.severity DESC    
-# '''
-#     cmd = cur.mogrify(tmpl, ())
-#     print_cmd(cmd)
-#     cur.execute(cmd)
-#     rows = cur.fetchall()
-#     show_table( rows, cols )
+    tmpl =  f'''
+    SELECT {c(cols)}
+      FROM Job_Posts
+     WHERE level = %s
+    '''
+    cmd = cur.mogrify(tmpl, (level,))
+    print_cmd(cmd)
+    cur.execute(cmd)
+    rows = cur.fetchall()
+    pp(rows)
+    show_table( rows, cols )
 
-create_job_tags()    
+create_job_tags('Internship')    
