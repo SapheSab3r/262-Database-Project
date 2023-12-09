6ef71aef0a5d2b6d8e3bd6536212198ab70f705f
sophiajwitt <sophiawitt33@gmail.com>
2023-12-08 19:04:16 -0500

    user story 2 complete

--- us2-count-applicants-simple-analytical.py
+++ us2-count-applicants-simple-analytical.py
@@ -10,22 +10,19 @@ So That:  I can analyze the level of interest and competitiveness for the positi
 
 print(us)
 
-def count_applicants():
+def count_applicants(jobID):
+    cols = 'applicationID'
 
-    # cols = 'b.bid b.severity i.initial_date i.product i.status i.priority u.uid u.name u.role'
+    tmpl =  f'''
+        SELECT COUNT(applicationID)
+        FROM Applications
+        WHERE jobID = %s   
+    '''
+    cmd = cur.mogrify(tmpl, (jobID,))
+    print_cmd(cmd)
+    cur.execute(cmd)
+    rows = cur.fetchall()
+    pp(rows)
+    show_table(rows, cols)
 
-#     tmpl =  f'''
-# SELECT {c(cols)}
-#   FROM Bugs as b
-#        JOIN Issues as i ON b.bid = i.iid
-#        JOIN Users as u ON i.uid = u.uid
-#  WHERE i.status <> 'resolved'
-#  ORDER BY b.severity DESC    
-# '''
-#     cmd = cur.mogrify(tmpl, ())
-#     print_cmd(cmd)
-#     cur.execute(cmd)
-#     rows = cur.fetchall()
-#     show_table( rows, cols )
-
-count_applicants()    
+count_applicants('J108')    
