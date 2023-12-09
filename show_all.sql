ac9c79f0dd7f2bd4b81523b7b70500fe03506f34
sophiajwitt <sophiawitt33@gmail.com>
2023-12-09 14:06:22 -0500

    updated and finished show_all.sql

--- show_all.sql
+++ show_all.sql
@@ -15,10 +15,24 @@ SELECT * FROM Job_Posts;
 SELECT * FROM Job_Seekers;
 \! echo "\n Notifications Table"
 SELECT * FROM Notifications;
+\! echo "\n Notified Table"
+
 SELECT * FROM Notified;
+\! echo "\n Received_Messages Table"
+
 SELECT * FROM Received_Messages;
+\! echo "\n Recruiters Table"
+
 SELECT * FROM Recruiters;
+\! echo "\n Saved_Jobs Table"
+
 SELECT * FROM Saved_Jobs;
+\! echo "\n Sent_Messages Table"
+
 SELECT * FROM Sent_Messages;
+\! echo "\n Submissions Table"
+
 SELECT * FROM Submissions;
+\! echo "\n Users Table"
+
 SELECT * FROM Users;
